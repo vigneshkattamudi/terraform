8c7c29a5617b9ac1b19727d302aaf1994d389ada
vigneshkattamudi <vkattamudi88@gmail.com>
2025-07-05 20:51:18 +0100

    terraform-ec2

---
+++ ec2.tf
@@ -0,0 +1,32 @@
+resource "aws_instance" "terraform-ec2" {
+  ami           = var.ami_id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-instance"
+  }
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow-all"
+  description = "Allow all traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  tags = {
+    Name = "allow_all"
+  }
+}
