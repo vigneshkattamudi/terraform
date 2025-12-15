d324401783af94ad432835e74152e1019432a9e9
vkatt@VIGNESH-PC <vkattamudi88@gmail.com>
2025-12-15 19:52:24 +0000

    terraform

---
+++ data-sources/data.tf
@@ -0,0 +1,19 @@
+data "aws_ami" "joindevops" {
+  most_recent = true
+  owners      = ["973714476881"]
+
+  filter {
+    name   = "name"
+    values = ["RHEL-9-DevOps-Practice*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
